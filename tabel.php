3946f74b20b3d3b63d263ab0a90b2b0f9ae74cc7
arijayadwi <arijayadwi@gmail.com>
2016-10-04 18:50:04 +0800

    mendapatkan user name

--- tabel.php
+++ tabel.php
@@ -5,6 +5,7 @@ $Key="0";
 <table cellspacing="2" cellpadding="2" border="1" id="tabel">
 	<tr align="center">
 	    <td><B>UserID</B></td>
+	    <td><B>User Name</B></td>
 	    <td width="200"><B>Tanggal & Jam</B></td>
 	    <td><B>Verifikasi</B></td>
 	    <td><B>Status</B></td>
@@ -24,27 +25,69 @@ $Key="0";
 		}
 	}else echo "Koneksi Gagal";
 	
+
 	include("parse.php");
 	$ID = array();
+	//$DateTime = "";
 	$buffer=Parse_Data($buffer,"<GetAttLogResponse>","</GetAttLogResponse>");
 	$buffer=explode("\r\n",$buffer);
-	for($a=0;$a<count($buffer);$a++){
+	for($a=count($buffer)-2;$a<=count($buffer)-2;$a++){
 		$data=Parse_Data($buffer[$a],"<Row>","</Row>");
 		$PIN=Parse_Data($data,"<PIN>","</PIN>");
 		$DateTime=Parse_Data($data,"<DateTime>","</DateTime>");
 		$Verified=Parse_Data($data,"<Verified>","</Verified>");
 		$Status=Parse_Data($data,"<Status>","</Status>");
+		//var_dump($PIN);
+		//array_push($ID, $PIN);
 		
-		array_push($ID, $PIN);
+		$Connect = fsockopen($IP, "80", $errno, $errstr, 1);
+		if($Connect){
+				$soap_request="<GetUserInfo><ArgComKey xsi:type=\"xsd:integer\">".$Key."</ArgComKey><Arg><PIN xsi:type=\"xsd:integer\">".$PIN."</PIN></Arg></GetUserInfo>";
+				$newLine="\r\n";
+				fputs($Connect, "POST /iWsService HTTP/1.0".$newLine);
+			    fputs($Connect, "Content-Type: text/xml".$newLine);
+			    fputs($Connect, "Content-Length: ".strlen($soap_request).$newLine.$newLine);
+			    fputs($Connect, $soap_request.$newLine);
+				$bufferUser="";
+				while($ResponseUser=fgets($Connect, 1024)){
+					$bufferUser=$bufferUser.$ResponseUser;
+					}
+			}else echo "Koneksi Gagal";
+			
+		
+
+		//include("parse.php");
+		$ID = array();
+		$bufferUser=Parse_Data($bufferUser,"<GetUserInfoResponse>","</GetUserInfoResponse>");
+		$bufferUser=explode("\r\n",$bufferUser);
+		$dataUser=Parse_Data($bufferUser[1],"<Row>","</Row>");
+		$PIN2=Parse_Data($dataUser,"<PIN>","</PIN>");
+		$Name=Parse_Data($dataUser,"<Name>","</Name>");
+
+
+		$timeStart = new DateTime($DateTime);
+		$timeNow = new DateTime("now");
+		//var_dump($timeNow);
+		// $diff = date_diff($timeStart, $timeNow);
+		// echo $diff->format("mm:ss");
+		// echo $timeNow;
+		// echo $DateTime;
+		// if($timeStart<$timeNow){
+		// 	echo "lalalal";
+		// }
+		$interval = date_create('now')->diff($DateTime);
+		print_r($interval);
+
 	
 	?>
 	<tr align="center">
 		    <td><?php echo $PIN?></td>
+		    <td><?php echo $Name?></td>
 		    <td><?=$DateTime?></td>
 		    <td><?=$Verified?></td>
 		    <td><?=$Status?></td>
 		</tr>
 	<?php }
-	//print_r($ID);
+
 	?>
 	</table>
